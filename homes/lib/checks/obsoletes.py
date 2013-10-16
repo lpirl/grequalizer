1dd24f558d7eda778fceb12a5106da2490c8686c
Lukas Pirl <git@lukas-pirl.de>
2013-10-16 17:12:50 +0200

    implemented correct() for obsoletes check

--- homes/lib/checks/obsoletes.py
+++ homes/lib/checks/obsoletes.py
@@ -1,5 +1,10 @@
+# encoding: utf-8
+
+from os import chmod
+from os.path import isfile, basename, dirname, join as path_join
+from shutil import make_archive
 from lib.checks import AbstractAllUsersAndAllDirectoriesCheck
-from lib.util import debug
+from lib.util import debug, log
 
 class ObsoletesCheck(AbstractAllUsersAndAllDirectoriesCheck):
     """
@@ -9,6 +14,16 @@ class ObsoletesCheck(AbstractAllUsersAndAllDirectoriesCheck):
     config_section = "obsoletes"
 
     order = 500
+
+    trash_path = None
+    octal_permissions = None
+
+    def post_init(self):
+        """
+        Stores some options as property for faster access.
+        """
+        self.trash_path = self.options.get_str('trash_path')
+        self.octal_permissions = self.options.get_int('trash_octal_permissions')
 
     def obsolete_directories(self, users, directories):
         """
@@ -22,11 +37,44 @@ class ObsoletesCheck(AbstractAllUsersAndAllDirectoriesCheck):
         """
         Checks correctness with a list of users and directories.
         """
-        return self.obsolete_directories(users, directories) == set()
+        return not bool(self.obsolete_directories(users, directories))
 
     def correct(self, users, directories):
         """
         Corrects home directory for a list of users and directories..
         """
         obsoletes = self.obsolete_directories(users, directories)
-        #TODO: go on here
+        for directory_path in obsoletes:
+            self.do_trash_directory(directory_path)
+
+    def trash_file_path(self, directory_path, suffix_number=0):
+        candidate = path_join(self.trash_path, basename(directory_path))
+
+        if suffix_number:
+            candidate += "_%u" % suffix_number
+
+        if isfile(candidate):
+            return self.trash_file_path(directory_path, suffix_number+1)
+        else:
+            return candidate
+
+    def do_trash_directory(self, directory_path):
+        """
+        Moves directory to trash.
+        """
+        trash_file_path = self.trash_file_path(directory_path)
+        debug("trashing directory '%s'" % directory_path)
+        archive_path = self.execute_safely(
+            make_archive,
+            trash_file_path,
+            "bztar",
+            dirname(directory_path),
+            basename(directory_path)
+        )
+        if archive_path:
+            self.execute_safely(    chmod,
+                                    trash_file_path,
+                                    self.octal_permissions)
+        else:
+            log(u"ERROR: something apparently went wrong - no archive " +
+                "file name found after archive creation!")

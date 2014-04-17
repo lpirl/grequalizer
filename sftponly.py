7978659ec15ab9b1bb139afaa76c40e711bf15f2
Lukas Pirl <git@lukas-pirl.de>
2014-04-17 15:39:38 +0200

    remove fallback to default configuration file

--- sftponly.py
+++ sftponly.py
@@ -5,18 +5,21 @@ This moduly mainly provides CLI interface.
 
 from sys import argv
 from lib import ChecksRunner
+from lib.util import log
+
+def help_and_exit():
+    log("script for maintaining users and ther chroots of a sftp-only server")
+    log("")
+    log("usage: [python3] ./sftponly.py [config file]")
+    log("  explicit call with 'python3' turns on debug")
+    exit(0)
 
 if __name__ == "__main__":
     if '--help' in argv or '-h' in argv:
-        log("script for maintaining users and ther chroots of a sftp-only server")
-        log("")
-        log("usage: [python3] ./sftponly.py [config file]")
-        log("  explicit call with 'python3' turns on debug")
-        log("  config file defaults to './sftponly.conf'")
-        log("    see this file for an example configuration")
-        exit(0)
+        help_and_exit()
+
     try:
         runner = ChecksRunner(argv[1])
     except IndexError as exception:
-        runner = ChecksRunner()
+        help_and_exit()
     runner.auto()
